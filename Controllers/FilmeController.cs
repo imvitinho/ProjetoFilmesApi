71dc9dffd57ba91bb5ba4bbb658fddafaf77c4ed
Victor M Martins <victormachado123martins@gmail.com>
2026-05-14 23:28:30 -0300

    Inserido o HttpGET para ler um recurso no sistema e adicionado o IEnumerable para seguimos a convenção de utilizar o verbo mais semântico

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -10,11 +10,17 @@ public class FilmeController : ControllerBase
 
     private static List<Filme> filmes = new List<Filme>();
 
-    [HttpPost]
+    [HttpPost] // Realiza a inserção de um recurso no sistema
     public void AdicionarFilme([FromBody] Filme filme)
     {
             filmes.Add(filme);
             Console.WriteLine(filme.Titulo);
             Console.WriteLine(filme.Duracao);
+    }
+
+    [HttpGet] // Realiza a leitura de um recurso no sistema
+    public IEnumerable<Filme> RecuperarFilmes()
+    {
+        return filmes;
     }
 }
