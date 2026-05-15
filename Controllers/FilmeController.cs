42b7e00484bc6ef4226f06d3ee0a012d1e4651cc
Victor M Martins <victormachado123martins@gmail.com>
2026-05-14 23:01:47 -0300

    Realizado parâmetros recebidos adicionado o Required e ErrorMensage

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -1,21 +1,20 @@
 ﻿using FilmesApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace FilmesApi.Controllers
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase 
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class FilmeController : ControllerBase 
+
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionarFilme([FromBody] Filme filme)
     {
-
-        private static List<Filme> filmes = new List<Filme>();
-
-        [HttpPost]
-        public void AdicionarFilme([FromBody]Filme filme)
-        {
             filmes.Add(filme);
             Console.WriteLine(filme.Titulo);
             Console.WriteLine(filme.Duracao);
-        }
     }
 }
