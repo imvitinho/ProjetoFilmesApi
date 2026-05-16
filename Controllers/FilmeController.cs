4031c0cd7ba348bb9af2f8421ef67692ebfca11e
Victor M Martins <victormachado123martins@gmail.com>
2026-05-16 00:38:27 -0300

    Realizada padronização de Paginação de resultados e Retorno

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -12,12 +12,13 @@ public class FilmeController : ControllerBase
     private static int id = 0;
 
     [HttpPost] // Realiza a inserção de um recurso no sistema
-    public void AdicionarFilme([FromBody] Filme filme)
+    public IActionResult AdicionarFilme([FromBody] Filme filme)
     {
-            filme.Id = id++;
+        filme.Id = id++;
         filmes.Add(filme);
-            Console.WriteLine(filme.Titulo);
-            Console.WriteLine(filme.Duracao);
+        return CreatedAtAction(nameof(RecuperaFilmePorId),
+            new { id = filme.Id },
+            filme);
     }
 
     [HttpGet] // Realiza a leitura de um recurso no sistema
@@ -27,8 +28,10 @@ public class FilmeController : ControllerBase
     }
 
     [HttpGet("{id}")]
-    public Filme? RecuperaFilmePorId(int id)
+    public IActionResult RecuperaFilmePorId(int id)
     {
-        return filmes.FirstOrDefault(filme => filme.Id == id);
+        var filme =  filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound(); // Retorna um código de status HTTP 404 (Not Found) se o filme não for encontrado
+        return Ok(filme); // Retorna um código de status HTTP 200 (OK) junto com o filme encontrado
     }
 }
